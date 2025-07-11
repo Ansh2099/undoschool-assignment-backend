848fc98a0658c00b9c7b4d4b7dedee57c3e51274
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-07-11 18:46:17 +0530

    feat: Added Config Settings for the ElasticSearch

---
+++ undoschool-assignment-backend/undoschool-assignment-backend/src/main/java/com/example/undoschool_assignment_backend/config/ElasticsearchConfig.java
@@ -0,0 +1,30 @@
+package com.example.undoschool_assignment_backend.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.JsonpMapper;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        // Register JavaTimeModule for ZonedDateTime support
+        ObjectMapper jackson = new ObjectMapper();
+        jackson.registerModule(new JavaTimeModule());
+
+        JsonpMapper jsonpMapper = new JacksonJsonpMapper(jackson);
+
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+        ElasticsearchTransport transport = new RestClientTransport(restClient, jsonpMapper);
+        return new ElasticsearchClient(transport);
+    }
+}
