45a486449eedc807fa088b256abfb128567427dd
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-07-11 18:43:35 +0530

    chore: added a .gitignore file and added the Application file

---
+++ undoschool-assignment-backend/undoschool-assignment-backend/src/main/java/com/example/undoschool_assignment_backend/UndoschoolAssignmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.undoschool_assignment_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndoschoolAssignmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndoschoolAssignmentBackendApplication.class, args);
+	}
+
+}
